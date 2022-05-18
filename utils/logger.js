fb77a254e6301416eb00b051ef0364f592ed0da3
Moritz Palm <moritz.palm@web.de>
2022-05-18 11:09:27 +0200

    added icons and dashboard page

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,7 @@ const winston = require("winston");
 const logger = winston.createLogger({
       level: 'info',
       format: winston.format.combine(
-          winston.format.label({ label: "Web app template" }),
+          winston.format.label({ label: "Weathertop app" }),
           // winston.format.timestamp(),
           winston.format.prettyPrint()
       ),
