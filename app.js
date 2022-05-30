1f7fb6056d3fa8fa5e24c66401db0347d13d4888
Moritz Palm <moritz.palm@web.de>
2022-05-30 10:04:27 +0200

    various fixes

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const logger = require("./utils/logger");
 const handlebars = require("express-handlebars");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: "This is a secret!",
+    cookie: {
+        maxAge: 3600000
+    },
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
@@ -15,10 +27,10 @@ app.set('views', './views');
 const routes = require("./routes");
 app.use("/", routes);
 
+
 app.listen(process.env.PORT, () => {
     console.log(`Web App listening on ${process.env.PORT}`);
 });
-const session = require("express-session");
 
 app.use(session({
     secret: "This is a secret!",
