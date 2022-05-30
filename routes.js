256acf448b8cac298b350bf011e5cfd0d094c279
Moritz Palm <moritz.palm@web.de>
2022-05-30 10:09:12 +0200

    added route protection

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("./utils/auth.js");
+
 
 const home = require("./controllers/home.js");
 const dashboard = require("./controllers/dashboard.js");
@@ -8,18 +10,17 @@ const accounts = require("./controllers/accounts.js");
 
 
 router.get("/", home.index);
-router.get("/dashboard", dashboard.index);
-router.get('/station/:id', station.index);
-router.get('/station/:id/deleterecording/:recordingId', station.deleteRecording);
-router.get('/dashboard/deletestation/:station_id', dashboard.deleteStation);
 router.get('/login', accounts.login);
 router.get('/signup', accounts.signup);
 router.get('/logout', accounts.logout);
 router.post('/register', accounts.register);
 router.post('/authenticate', accounts.authenticate);
 
-
-router.post('/station/:id/addrecording', station.addRecording);
-router.post('/dashboard/addstation', dashboard.addStation);
+router.get("/dashboard", auth.protected, dashboard.index);
+router.get('/dashboard/deletestation/:station_id', auth.protected, dashboard.deleteStation);
+router.post('/dashboard/addstation', auth.protected, dashboard.addStation);
+router.get('/station/:id', auth.protected, station.index);
+router.get('/station/:id/deleterecording/:recordingId', auth.protected, station.deleteRecording);
+router.post('/station/:id/addrecording', auth.protected, station.addRecording);
 
 module.exports = router;
